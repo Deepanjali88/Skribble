b7fd15a3ee8d20781428067b1fe74288297fc196
Varshitha <varshitha.u.4@gmail.com>
2023-08-22 21:59:27 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,13 @@ import 'package:skribble/paint_screen.dart';
 // void main() {
 //   runApp(const MyApp());
 // }
-//
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
 //       title: 'Flutter Demo',
 //       theme: ThemeData(
-//
 //         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
 //         useMaterial3: true,
 //       ),
@@ -30,8 +26,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
